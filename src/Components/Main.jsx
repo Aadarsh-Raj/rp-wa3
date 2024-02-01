22e7aa395ff800eef0dc68deb2cb7bfd0c442dfa
Aadarsh Raj <aadarshraj06062001@gmail.com>
2024-02-01 13:03:26 +0530

    updated button

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,48 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Header from './Header';
-import Card from './Card';
-import './Style/main.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Header from "./Header";
+import Card from "./Card";
+import "./Style/main.css";
 function Main() {
-const [data, setData] = useState([]);
-const [page, setPage] = useState(1);
-const [loading, setLoading] = useState(false);
-const [hasMore, setHasMore] = useState(true);
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(5);
+  const [loading, setLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
 
-useEffect(()=>{
+  const fetchedData = async (page) => {
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        `https://api.unsplash.com/photos/?page=${page}&client_id=fnRGxxXp2xykiYtoqXKec1Y02FY_IE88i9sbYszSu2Q`
+      );
+      const newData = response.data;
+      setData(newData);
+      setLoading(false);
 
-    const fetchedData = async ()=>{
-        try {
-            setLoading(true);
-            const response = await axios.get("https://api.unsplash.com/photos/?page=2&per_page=20&client_id=fnRGxxXp2xykiYtoqXKec1Y02FY_IE88i9sbYszSu2Q");
-            const newData = response.data;
-            setData(newData);
-            setLoading(false);
-
-            if(newData.length === 0){
-                setHasMore(false);
-            }
-        } catch (error) {
-            console.log(error);
-        }
+      if (newData.length === 0) {
+        setHasMore(false);
+      }
+    } catch (error) {
+      console.log(error);
     }
-    fetchedData();
+  };
 
-},[page])
-
-
+  const handleScroll = () => {
+      console.log("Arya", page);
+      setPage((prevValue) => prevValue + 1);
+      console.log(page)
+      fetchedData(page);
+  };
+  useEffect(() => {
+    fetchedData(page);
+  }, [handleScroll]);
 
   return (
     <>
-    <Header />
-    <section className="card-container">
-        {
-            data.map((ele)=>(<Card key={ele.id} fetchedData={ele} />))
-        }
-    </section>
-    
+      <Header />
+      
+      <section className="card-container" >
+        <button type="button" onClick={handleScroll}>Load more</button>
+        {data.map((ele) => (
+          <Card key={ele.id} fetchedData={ele} />
+        ))}
+      </section>
     </>
-  )
+  );
 }
 
-export default Main
+export default Main;
